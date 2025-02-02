a71031c2a3046b1f689f9aec8f534937db47c204
granttnarg <48766845+granttnarg@users.noreply.github.com>
2025-02-02 22:47:13 +0100

    basic setup for controller and views to make short urls

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,44 @@
+class UrlsController < ApplicationController
+  def redirect
+    @url = find_url
+    if @url.nil? || !@url.valid_url?
+      not_found
+    else
+      page = @url.original
+    end
+
+    redirect_to page, allow_other_host: true
+  end
+
+  def show
+    @url = find_url
+  end
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.new(url_params)
+
+    if @url.save
+      redirect_to url_path(@url), notice: 'URL was successfully created'
+    else
+      render :new, status: :unprocessable_entity, notice: "Url Generation Failed"
+    end
+  end
+
+  def url_params
+    params.require(:url).permit(:original, :slug).reject { |_, v| v.blank? }
+  end
+
+  private
+
+  def find_url
+    Url.find_by_slug!(params[:id])
+  end
+
+  def not_found
+    raise ActionController::RoutingError.new('Url Not Found')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :url do
-    member do
-      get ':slug', to: 'url#show'
-    end
-  end
+  get "/:id", to: "urls#redirect"
+
+  resources :urls, only: [:new, :create, :show, :update]
 end
